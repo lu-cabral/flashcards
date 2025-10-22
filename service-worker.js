ce525d5b0adb44992ce43e3d0c843da1487e3547
Luana <luanacabralrib@gmail.com>
2025-10-22 07:18:22 -0300

    feat: adiciona suporte a PWA (manifest, service worker e ícones)

---
+++ service-worker.js
@@ -0,0 +1,29 @@
+const CACHE_NAME = 'flashcards-cache-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/css/styles.css',
+  '/js/app.js',
+  '/manifest.json',
+  '/icons/icon-192.png',
+  '/icons/icon-512.png',
+  'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css'
+];
+
+self.addEventListener('install', function(event) {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(function(cache) {
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request)
+      .then(function(response) {
+        return response || fetch(event.request);
+      })
+  );
+});
